bf99cee61fd5b8ee6472bc9e7ff6cd99f3db0073
Michele Bertasi <michele.bertasi@gmail.com>
2015-10-25 20:24:04 +0100

    Added vsftpd configuration

--- configure.sh
+++ configure.sh
@@ -2,6 +2,8 @@
 
 # settings
 USER=michele
+FTP_USER=ftpr
+FTP_USER_HOME=/home/ftpr
 
 # packages
 apt-get update
@@ -16,6 +18,36 @@ dpkg-reconfigure locales
 # add admin user
 adduser $USER
 usermod -a -G sudo $USER
+
+# ftp
+apt-get install -y vsftpd
+cat >/etc/vsftpd.conf <<EOL
+# enable only this list of users
+# the file format is one user per line
+userlist_enable=YES
+userlist_deny=NO
+userlist_file=/etc/vsftpd.userlist
+
+# chroot for these users
+chroot_local_user=YES
+chroot_list_enable=YES
+# (default follows)
+chroot_list_file=/etc/vsftpd.chroot_list
+
+# workaround to not enable seccomp
+# enable this only if your machine does not support seccomp
+seccomp_sandbox=no
+EOL
+
+cat $FTP_USER >/etc/vsftpd.userlist
+cat $FTP_USER >/etc/vsftpd.chroot_list
+
+# ftp user
+useradd -d $FTP_USER_HOME $FTP_USER
+mkdir $FTP_USER_HOME
+chown $FTP_USER:$FTP_USER $FTP_USER_HOME
+# start vsftpd
+service vsftpd reload
 
 # user setup
 su $USER
